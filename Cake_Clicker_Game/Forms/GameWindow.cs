add244c6300844f78283f90085389332aac7d5a3
Tyler Watson <49106114+Magicboys@users.noreply.github.com>
2021-02-28 20:40:30 -0800

    UI for Achievement update

--- Cake_Clicker_Game/Forms/GameWindow.cs
+++ Cake_Clicker_Game/Forms/GameWindow.cs
@@ -22,6 +22,12 @@ namespace Cake_Clicker_Game
             //refreshTimer.Start();
             cakeButton.MouseDown += new MouseEventHandler(Shrink);
             cakeButton.MouseUp += new MouseEventHandler(Grow);
+
+            //Mask the Achievement descriptions
+            Achievement_One_Description.Text = "????????????????";
+            Achievement_Two_Description.Text = "????????????????";
+            Achievement_Three_Description.Text = "????????????????";
+            Achievement_Four_Description.Text = "????????????????";
         }
 
         /// <summary>
@@ -52,6 +58,7 @@ namespace Cake_Clicker_Game
         private void cakeButton_Click(object sender, EventArgs e)
         {
             _userInterfaceManager.OnCakeClick();
+            CheckAchievements();
             //addPointsTimer.Interval = 1000;
             //addPointsTimer.Start();
         }
@@ -82,40 +89,43 @@ namespace Cake_Clicker_Game
         private void addVanillaCakeButton_Click(object sender, EventArgs e)
         {
             _userInterfaceManager.AddCake(Game.CakeType.Vanilla);
+            CheckAchievements();
         }
 
         private void addChocolateCakeButton_Click(object sender, EventArgs e)
         {
             _userInterfaceManager.AddCake(Game.CakeType.Chocolate);
+            CheckAchievements();
         }
 
         private void addStrawberryCakeButton_Click(object sender, EventArgs e)
         {
             _userInterfaceManager.AddCake(Game.CakeType.Strawberry);
+            CheckAchievements();
         }
 
         private void addCoffeeCakeButton_Click(object sender, EventArgs e)
         {
             _userInterfaceManager.AddCake(Game.CakeType.Coffee);
-
+            CheckAchievements();
         }
 
         private void addRedVelvetCakeButton_Click(object sender, EventArgs e)
         {
             _userInterfaceManager.AddCake(Game.CakeType.Red_Velvet);
-
+            CheckAchievements();
         }
 
         private void addCarrotCakeButton_Click(object sender, EventArgs e)
         {
             _userInterfaceManager.AddCake(Game.CakeType.Carrot);
-
+            CheckAchievements();
         }
 
         private void addCheeseCakeButton_Click(object sender, EventArgs e)
         {
             _userInterfaceManager.AddCake(Game.CakeType.Cheese);
-
+            CheckAchievements();
         }
 
         #endregion
@@ -126,6 +136,45 @@ namespace Cake_Clicker_Game
             Application.Exit();
         }
 
+        //This method is called when an achievement has the potential of being unlocked
+        //For example, when the user buys an upgrade or clicks to get more cake
+        private void CheckAchievements() 
+        {
+            bool[] values = null;
+            //Calls game class method to get the array of achievement status's
+            if (values[0] == true) 
+            {
+                Achievement_One_Groupbox.Text = "Cake Novice";
+                Achievement_One_Description.Text = "Reached 100 Cakes!";
+                Achievement_One_Description.ForeColor = Color.Gold;
+                Achievement_One_Question_Mark.Visible = false;
+                Achievement_One_Star.Visible = true;
+            } 
+            else if (values[1] == true)
+            {
+                Achievement_Two_Groupbox.Text = "Cake Novice";
+                Achievement_Two_Description.Text = "Reached 1000 Cakes!";
+                Achievement_Two_Description.ForeColor = Color.Gold;
+                Achievement_Two_Question_Mark.Visible = false;
+                Achievement_Two_Star.Visible = true;
+            }
+            else if (values[2] == true)
+            {
+                Achievement_Three_Groupbox.Text = "Cake Novice";
+                Achievement_Three_Description.Text = "Reached 10000 Cakes!";
+                Achievement_Three_Description.ForeColor = Color.Gold;
+                Achievement_Three_Question_Mark.Visible = false;
+                Achievement_Three_Star.Visible = true;
+            }
+            else if (values[3] == true)
+            {
+                Achievement_Four_Groupbox.Text = "Cake Novice";
+                Achievement_Four_Description.Text = "Unlocked all upgrades!";
+                Achievement_Four_Description.ForeColor = Color.Gold;
+                Achievement_Four_Question_Mark.Visible = false;
+                Achievement_Four_Star.Visible = true;
+            }
+        }
 
     }
 }
